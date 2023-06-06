777056f805a313b771c2afebff9f04e3bdcd20db
Pybounce <pybounce@protonmail.com>
2023-06-07 00:53:32 +0100

    Removed custom stage query and default stage

--- Assets/Databases/ResourceJSONDBMS.cs
+++ Assets/Databases/ResourceJSONDBMS.cs
@@ -8,25 +8,10 @@ using UnityEngine;
 public class ResourceJSONDBMS : IReadIDBMS
 {
 
-    public static StageData LoadCustomStage(int stageId)
-    {
-        try
-        {
-            string stagePath = Application.persistentDataPath + "Stages/Stage_" + stageId;
-            string fileContents = File.ReadAllText(stagePath);
-            return JsonUtility.FromJson<StageData>(fileContents);
-        }
-        catch
-        {
-            return DefaultStage;
-        }
-    }
-
     public T Load<T>(string directory)
     {
         TextAsset jsonText = Resources.Load<TextAsset>(directory);
         return JsonUtility.FromJson<T>(jsonText.text);
     }
 
-    public static StageData DefaultStage = new StageData(-1, new PropData[1], 1, Vector3.zero);
 }
