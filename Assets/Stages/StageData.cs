ab69c4b1f78c4ee1c9bbe00b90d8443cc88bcebe
Pybounce <pybounce@protonmail.com>
2023-06-07 00:16:10 +0100

    Removed comments

--- Assets/Stages/StageData.cs
+++ Assets/Stages/StageData.cs
@@ -17,37 +17,3 @@ public struct StageData
     }
 
 }
-    /*
-    public StageData(StageJsonData jsonData, int id)
-    {
-        int width = jsonData.PropDataWidth;
-        int height = jsonData.PropData.Length / width;
-
-        PropData[,] propData = new PropData[height, width];
-        for (int x = 0; x < width; x++)
-        {
-            for (int y = 0; y < height; y++)
-            {
-                int index = (y * width) + x;
-                propData[y, x] = jsonData.PropData[index];
-            }
-        }
-        Id = id;
-        PropData = propData;
-        PlayerStartPosition = jsonData.PlayerStartPosition;
-    }
-}
-
-[System.Serializable]
-public struct StageJsonData
-{
-    public PropData[] PropData;
-    public Vector3 PlayerStartPosition;
-    public int PropDataWidth;
-    public StageJsonData(PropData [] tiles, Vector3 playerStartPosition, int propDataWidth)
-    {
-        PropData = tiles;
-        PlayerStartPosition = playerStartPosition;
-        PropDataWidth = propDataWidth;
-    }
-}*/
