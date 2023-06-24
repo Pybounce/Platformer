b0b924d57bd66921ac39aad3fc5485d00f162eb8
Pybounce <pybounce@protonmail.com>
2023-06-24 03:29:29 +0100

    Player can now collide with obstacle/completion item once per frame max

--- Assets/Player/PlayerCollisionController.cs
+++ Assets/Player/PlayerCollisionController.cs
@@ -7,17 +7,26 @@ public class PlayerCollisionController : MonoBehaviour
 {
     [SerializeField] private LayerMask ObstacleLayerMask;
     [SerializeField] private LayerMask CompletionGemLayerMask;
+    private bool _hasCollidedThisFrame = false;
+
+    private void Update()
+    {
+        _hasCollidedThisFrame = false;
+    }
 
     private async void OnTriggerEnter(Collider collision)
     {
+        if (_hasCollidedThisFrame) { return; }
         if (1 << collision.gameObject.layer == ObstacleLayerMask)
         {
+            _hasCollidedThisFrame = true;
             GetComponent<PlayerController>().Kill();
             await Wait();
             StageManager.Instance.RestartStage();
         }
         else if (1 << collision.gameObject.layer == CompletionGemLayerMask)
         {
+            _hasCollidedThisFrame = true;
             StageManager.Instance.CompleteStage();
         }
     }
