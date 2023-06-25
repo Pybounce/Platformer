ce0be5ddd119b1a849e75d45107f4dd4dac20c36
Pybounce <pybounce@protonmail.com>
2023-06-25 03:21:02 +0100

    Supports multiple layers for movement and objects. Changes to movement data packing.

--- Assets/Scripts/Level Editing/LevelExtractor.py
+++ Assets/Scripts/Level Editing/LevelExtractor.py
@@ -98,62 +98,73 @@ def GetMoverInput(colour):
         return moverInput
     moverInput.Enabled = True
     moverInput.Offset = Vec3(colour[0] / 10.0, colour[1] / 10.0, 0)
-    moverInput.Speed = colour[2] / 10.0
-    if (colour[3] < 100):
-        moverInput.Offset = Vec3(-moverInput.Offset.x, -moverInput.Offset.y, -moverInput.Offset.z)
+    if (colour[2] < 100):
+      moverInput.Speed = colour[2] / 10.0
+    else:
+      moverInput.Speed = (colour[2] - 100) / 10.0
+      moverInput.Offset = Vec3(-moverInput.Offset.x, -moverInput.Offset.y, -moverInput.Offset.z)
+    moverInput.StartLerp = (colour[3] - 100) / 100
     return moverInput
 
 levelIndex = input("Stage Index: ")
-mainReadPath = r"C:\Users\Michael\Desktop\Platformer_3D\Stages\Aseprite\Stage_" + levelIndex + r".png"
-mainImage = Image.open(mainReadPath) # Can be many different formats.
-mainPix = mainImage.load()
-
-movementReadPath = r"C:\Users\Michael\Desktop\Platformer_3D\Stages\Aseprite\Stage_" + levelIndex + r"_m.png"
-movement_exists = exists(movementReadPath)
-if (movement_exists):
-    movementImage = Image.open(movementReadPath) # Can be many different formats.
-    movementPix = movementImage.load()
-
-
-
 writePath = r"C:\Users\Michael\Unity Projects\Platformer_3D\Assets\Resources\Stages\Stage_" + levelIndex + r".json"
 
+preLoadMainForSize = Image.open(r"C:\Users\Michael\Desktop\Platformer_3D\Stages\Aseprite\Stage_" + levelIndex + "_0" + r".png")
 
 levelData = LevelData()
-levelData.Size = Vec2(mainImage.width, mainImage.height)
+levelData.Size = Vec2(preLoadMainForSize.width, preLoadMainForSize.height)
 levelData.PropData = []
 
 
-for y in range(mainImage.height):
-  for x in range(mainImage.width):
-    mainColour = mainPix[x, y]
-    mainColourRGB = (mainColour[0], mainColour[1], mainColour[2])
-    mainAlpha = mainColour[3]
-    gridIndex = Vec2(x, y)
-    newProp = PropData(0, gridIndex, 0)
+currentSubStageIndex = 0
 
-    if (mainColour == (0, 0, 0, 255)): 				#Ground
-      newProp = PropData(1, gridIndex, 0)
-    elif (mainColourRGB == (255, 0, 0)):			#Sawblade
-      newProp = PropData(5, gridIndex, 0)
-    elif (mainColourRGB == (255, 255, 0)):			#Spike Double
-      newProp = PropData(4, gridIndex, 0)
-    elif (mainColourRGB == (255, 100, 0)):			#Sawblade Block
-      newProp = PropData(6, gridIndex, 0)
-    elif (mainColourRGB == (0, 255, 0)):			#Sawblade_02
-      newProp = PropData(7, gridIndex, 0)
-    elif (mainColourRGB == (255, 255, 255)):		#Completion Item
-      newProp = PropData(3, gridIndex, 0)
-    elif (mainColourRGB == (0, 0, 255)):			#Player
-      levelData.PlayerStartIndex = Vec2(x, y)
-      continue
-    else:    							#Air
-      continue
-    if (movement_exists):
-      newProp.MoverInput = GetMoverInput(movementPix[x, y])
+while (True):
+  mainReadPath = r"C:\Users\Michael\Desktop\Platformer_3D\Stages\Aseprite\Stage_" + levelIndex + "_" + str(currentSubStageIndex) + r".png"
+  main_exists = exists(mainReadPath)
+  if (main_exists == False):
+      break
+  mainImage = Image.open(mainReadPath) # Can be many different formats.
+  mainPix = mainImage.load()
+  
+  movementReadPath = r"C:\Users\Michael\Desktop\Platformer_3D\Stages\Aseprite\Stage_" + levelIndex  + "_" + str(currentSubStageIndex) + r"_m.png"
+  movement_exists = exists(movementReadPath)
+  if (movement_exists):
+      movementImage = Image.open(movementReadPath) # Can be many different formats.
+      movementPix = movementImage.load()
+  
+  
+  for y in range(mainImage.height):
+    for x in range(mainImage.width):
+      mainColour = mainPix[x, y]
+      mainColourRGB = (mainColour[0], mainColour[1], mainColour[2])
+      mainAlpha = mainColour[3]
+      gridIndex = Vec2(x, y)
+      newProp = PropData(0, gridIndex, 0)
+  
+      if (mainColour == (0, 0, 0, 255)): 				#Ground
+        newProp = PropData(1, gridIndex, 0)
+      elif (mainColourRGB == (255, 0, 0)):			#Sawblade
+        newProp = PropData(5, gridIndex, 0)
+      elif (mainColourRGB == (255, 255, 0)):			#Spike Double
+        newProp = PropData(4, gridIndex, 0)
+      elif (mainColourRGB == (255, 100, 0)):			#Sawblade Block
+        newProp = PropData(6, gridIndex, 0)
+      elif (mainColourRGB == (0, 255, 0)):			#Sawblade_02
+        newProp = PropData(7, gridIndex, 0)
+      elif (mainColourRGB == (255, 255, 255)):		#Completion Item
+        newProp = PropData(3, gridIndex, 0)
+      elif (mainColourRGB == (0, 0, 255)):			#Player
+        levelData.PlayerStartIndex = Vec2(x, y)
+        continue
+      else:    							#Air
+        continue
+      if (movement_exists):
+        newProp.MoverInput = GetMoverInput(movementPix[x, y])
+  
+      newProp.Direction = AlphaToRot(mainAlpha)
+      levelData.PropData.append(newProp.toJson())
+  currentSubStageIndex += 1
 
-    newProp.Direction = AlphaToRot(mainAlpha)
-    levelData.PropData.append(newProp.toJson())
 
 
 with open(writePath, "w") as outfile:
